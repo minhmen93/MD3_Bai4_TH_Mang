360bf713f8252cb9199c7670222539adfc1f9d09
minhmen93 <119550691+minhmen93@users.noreply.github.com>
2023-03-10 12:15:55 +0700

    Add files via upload

---
+++ MD3_Bai4_ThucHanh/src/ThucHanh.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ThucHanh {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the width:");
+        double width = scanner.nextDouble();
+        System.out.print("Enter the height:");
+        double height = scanner.nextDouble();
+        HinhChuNhat rectangle = new HinhChuNhat(width, height);
+        System.out.println("Your Rectangle \n"+ rectangle.display());
+        System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+}
+
